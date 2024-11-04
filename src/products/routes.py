6bd77f37534e6e59cd9e4289c866bca3e6be2af5
leejungyan <119379713+leejungyan08@users.noreply.github.com>
2024-11-04 17:43:30 +0800

    update

--- src/products/routes.py
+++ src/products/routes.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Form, File, UploadFile
 from fastapi.exceptions import HTTPException
 from typing import List
 from src.products.schemas import Product
 from sqlalchemy.orm import Session
 from src.database.models import PRODUCTS
 from src.database.main import get_db
+from sqlalchemy.orm import Session
+import os
+from src.products.schemas import OptionGroup 
 product_router = APIRouter()
 
 
@@ -16,4 +19,27 @@ async def get_products(db:Session = Depends(get_db)):
 @product_router.post('/create_transaction')
 async def create_transaction(db:Session = Depends(get_db)):
     return 'success'
+
+@product_router.post('/product/create')
+async def create_product(name: str = Form(...),
+    description: str = Form(...),
+    category: str = Form(...),
+    price: float = Form(...),
+    file: UploadFile = File(...),
+    optionGroups: List[str] = Form(...),
+    db: Session = Depends(get_db)):
+    # save_path = os.path.join(os.getcwd(), file.filename) 
+    # with open(save_path, "wb") as f:
+    #     contents = await file.read()  # Read the file contents
+    #     f.write(contents)  # Write contents to the file
+    option_groups = [OptionGroup.parse_raw(group) for group in optionGroups]
+    return option_groups
+    # return {
+    #     "status": "success",
+    #     "name": name,
+    #     "description": description,
+    #     "category": category,
+    #     "price": price,
+    #     "optionGroups": optionGroups
+    # }
 
--- src/products/schemas.py
+++ src/products/schemas.py
@@ -1,7 +1,26 @@
 from pydantic import BaseModel
-
+from typing import List
+from pydantic import BaseModel, Field
 class Product(BaseModel):
     ID:int
     NAME:str
     CATEGORY:str
     PRICE:int
+
+class Option(BaseModel):
+    option: str
+    desc: str
+    price: str
+
+class OptionGroup(BaseModel):
+    name: str
+    collapse: bool
+    default: str
+    options: List[Option]
+
+class ProductCreateRequest(BaseModel):
+    name: str
+    description: str
+    category: int
+    price: float
+    optionGroups: List[OptionGroup] = Field(default_factory=list)
